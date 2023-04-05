2fe2a60b3e894d03fafddea088df5dd33780acf4
JesseTDev <121024534+JesseTDev@users.noreply.github.com>
2023-04-05 16:34:08 +1000

    updates to trivia page styling and setup context

--- src/Components/landing-page/landing-page.component.jsx
+++ src/Components/landing-page/landing-page.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+const LandingPageContainer = styled.div `
+    
+`
 const LandingPage = () => {
     return (
-        <div>
-
-        </div>
+      <LandingPageContainer>
+        <h1>Welcome To Trivia!</h1>
+      </LandingPageContainer>
     );
 }; 
 
--- src/TriviaApp.component.jsx
+++ src/TriviaApp.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 // imported components 
-
+import LandingPage from "./Components/landing-page/landing-page.component";
 // styled components 
 const AppContainer = styled.div `
     
@@ -10,7 +10,7 @@ const AppContainer = styled.div `
 const TriviaApp = () => {
     return (
     <AppContainer>
-        
+        <LandingPage /> 
     </AppContainer>
     );
 };
